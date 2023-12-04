c99087bef51269d0aca0cb05aa42f4e2255ab225
junya koyama <arukiidou@yahoo.co.jp>
2023-12-04 23:37:54 +0900

    feat: Add unit tests.
    Signed-off-by: junya koyama <arukiidou@yahoo.co.jp>

--- src/test/java/nablarch/integration/micrometer/otlp/NablarchOtlpConfigTest.java
+++ src/test/java/nablarch/integration/micrometer/otlp/NablarchOtlpConfigTest.java
@@ -1,9 +1,12 @@
 package nablarch.integration.micrometer.otlp;
 
+import io.micrometer.registry.otlp.OtlpConfig;
+import nablarch.core.repository.di.DiContainer;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThrows;
 
 /**
  * {@link NablarchOtlpConfig}の単体テスト。
@@ -14,6 +17,27 @@ public class NablarchOtlpConfigTest {
     @Test
     public void testSubPrefix() {
         NablarchOtlpConfig sut = new NablarchOtlpConfig(null, null);
+        // From NablarchOtlpConfig
         assertThat(sut.subPrefix(), is("otlp"));
+    }
+
+    /**
+     * {@link nablarch.integration.micrometer.NablarchMeterRegistryConfig}のデフォルト値テスト。
+     */
+    @Test
+    public void testDefaultFromNablarchMeterRegistryConfig() {
+        NablarchOtlpConfig sut = new NablarchOtlpConfig(null, null);
+        assertThat(sut.prefix(), is(String.join(".", "nablarch.micrometer", sut.subPrefix())));
+        assertThat(sut.prefix(), is("nablarch.micrometer.otlp"));
+    }
+
+    /**
+     * {@link io.micrometer.registry.otlp.OtlpConfig}のデフォルト値テスト。
+     */
+    @Test
+    public void testDefaultFromOtlpConfig() {
+        // diContainerをnullにすると、NullPointerException
+        NablarchOtlpConfig sut = new NablarchOtlpConfig(null, null);
+        assertThrows(NullPointerException.class, sut::url);
     }
 }
--- src/test/java/nablarch/integration/micrometer/otlp/OtlpMeterRegistryFactoryTest.java
+++ src/test/java/nablarch/integration/micrometer/otlp/OtlpMeterRegistryFactoryTest.java
@@ -6,6 +6,9 @@ import nablarch.core.repository.disposal.BasicApplicationDisposer;
 import nablarch.integration.micrometer.DefaultMeterBinderListProvider;
 import org.junit.Test;
 
+import java.time.Duration;
+import java.util.Collections;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
@@ -15,6 +18,10 @@ import static org.hamcrest.Matchers.is;
  */
 public class OtlpMeterRegistryFactoryTest {
 
+    /**
+     * 空ではないpropertiesファイルを設定した時のテストケース。
+     * @author Junya Koyama
+     */
     @Test
     public void testCreateObject() {
         OtlpMeterRegistryFactory sut = new OtlpMeterRegistryFactory();
@@ -26,6 +33,44 @@ public class OtlpMeterRegistryFactoryTest {
         OtlpMeterRegistry meterRegistry = sut.createObject();
 
         NablarchOtlpConfig config = Deencapsulation.getField(meterRegistry, "config");
+        assertThat(config.url(), is("http://localhost:9090/api/v1/otlp/v1/metrics"));
+
+        // From NablarchMeterRegistryConfig
+        assertThat(config.prefix(), is("test.otlp"));
+        //From OtlpConfig
+        assertThat(config.resourceAttributes(), is(Collections.<String, String>emptyMap()));
+        // From PushRegistryConfig
+        assertThat(config.step(), is(Duration.ofMinutes(1L)));
+        assertThat(config.enabled(), is(true));
+        assertThat(config.batchSize(), is(10000));
+    }
+
+    /**
+     * 空のpropertiesファイルを設定し、環境変数も設定しない時のテストケース。
+     * @author Junya Koyama
+     */
+    @Test
+    public void testDefault() {
+        // NablarchOtlpConfigは継承関係が複雑なので、デフォルト値の挙動をテストで示しておく。
+        OtlpMeterRegistryFactory sut = new OtlpMeterRegistryFactory();
+        sut.setApplicationDisposer(new BasicApplicationDisposer());
+        sut.setMeterBinderListProvider(new DefaultMeterBinderListProvider());
+        sut.setXmlConfigPath("nablarch/integration/micrometer/otlp/OtlpMeterRegistryFactory/testCreateObject/test.empty.xml");
+
+        OtlpMeterRegistry meterRegistry = sut.createObject();
+        NablarchOtlpConfig config = Deencapsulation.getField(meterRegistry, "config");
+
+        // From NablarchOtlpConfig
+        assertThat(config.subPrefix(), is("otlp"));
+        // From NablarchMeterRegistryConfig
+        assertThat(config.prefix(), is(String.join(".", "nablarch.micrometer", config.subPrefix())));
+        assertThat(config.prefix(), is("nablarch.micrometer.otlp"));
+        // From OtlpConfig
         assertThat(config.url(), is("http://localhost:4318/v1/metrics"));
+        assertThat(config.resourceAttributes(), is(Collections.<String, String>emptyMap()));
+        // From PushRegistryConfig
+        assertThat(config.step(), is(Duration.ofMinutes(1L)));
+        assertThat(config.enabled(), is(true));
+        assertThat(config.batchSize(), is(10000));
     }
 }
